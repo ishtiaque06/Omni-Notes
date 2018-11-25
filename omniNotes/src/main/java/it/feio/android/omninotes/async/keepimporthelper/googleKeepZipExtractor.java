a12d82e43f64221b501ff5eadadd854e100ea696
Ahmed Ishtiaque <ahmeidishti27@gmail.com>
2018-11-25 12:01:40 -0500

    Completed ZIP extractor and test suite. ZIP extractor now returns ArrayList<Document>

--- omniNotes/src/main/java/it/feio/android/omninotes/async/keepimporthelper/googleKeepZipExtractor.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/async/keepimporthelper/googleKeepZipExtractor.java
@@ -1,26 +1,37 @@
 package it.feio.android.omninotes.async.keepimporthelper;
 
 import org.jsoup.Jsoup;
-import org.w3c.dom.Document;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 public class googleKeepZipExtractor {
     private String mPath;
     private ZipInputStream mZip;
-    private ArrayList<Document> mNotesList;
+    private ZipFile mZipToExtract;
+    private ArrayList<Document> mNotesList = new ArrayList<>();
 
     // Creates a ZipInputStream from a given path
     public googleKeepZipExtractor(String path) {
         mPath = path;
         try {
             mZip = new ZipInputStream(new FileInputStream(mPath));
-        } catch (FileNotFoundException e) {
+            mZipToExtract = new ZipFile(mPath);
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -30,7 +41,7 @@ public class googleKeepZipExtractor {
     public boolean verifyKeepFolder() throws IOException {
         ZipEntry notesFolder = mZip.getNextEntry();
         while (notesFolder != null) {
-            String name = notesFolder.getName().replaceFirst("\\w*/", "");
+            String name = this.truncateFilePath(notesFolder);
             if ((name.length() >= 5) && (name.substring(0, 4).equals("Keep"))) {
                 mZip.closeEntry();
                 mZip.close();
@@ -45,8 +56,42 @@ public class googleKeepZipExtractor {
 
     // Returns an ArrayList of Document objects to make notes out of.
     // minimum length of a valid HTML filename: "Keep/h.html" == 11
-//    public ArrayList<Document> returnHtmlKeepNotes() throws IOException {
-//        ZipEntry notesFolder = mZip.getNextEntry();
-//
-//    }
+    public ArrayList<Document> returnHtmlKeepNotes() throws IOException {
+        if (!this.verifyKeepFolder()) {
+            return mNotesList;
+        }
+        try {
+            Enumeration<? extends ZipEntry> entries = mZipToExtract.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry noteEntry = entries.nextElement();
+                String name = this.truncateFilePath(noteEntry);
+                if ((name.length() >= 11)
+                        && (name.substring(0, 4).equals("Keep"))
+                        && (name.substring(name.length() - 5, name.length()).equals(".html"))) {
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(mZipToExtract.getInputStream(noteEntry))
+                            );
+                    String line;
+                    StringBuilder sb = new StringBuilder();
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                    }
+                    br.close();
+                    String result = sb.toString();
+                    if (result.length() != 0) {
+                        Document doc = Jsoup.parse(result);
+                        mNotesList.add(doc);
+                    }
+                }
+            }
+        }
+        finally {
+            mZipToExtract.close();
+        }
+        return mNotesList;
+    }
+
+    private String truncateFilePath(ZipEntry entry) {
+        return entry.getName().replaceFirst("\\w*/", "");
+    }
 }
--- omniNotes/src/test/java/it/feio/android/omninotes/async/keepimporthelper/googleKeepZipExtractorTest.java
+++ omniNotes/src/test/java/it/feio/android/omninotes/async/keepimporthelper/googleKeepZipExtractorTest.java
@@ -1,9 +1,13 @@
 package it.feio.android.omninotes.async.keepimporthelper;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -15,6 +19,8 @@ public class googleKeepZipExtractorTest {
     URL notKeep = classLoader.getResource("dummyZipNotKeep.zip");
     URL Keep = classLoader.getResource("dummyZipKeep.zip");
     URL keepWithData = classLoader.getResource("keepTakeoutTest.zip");
+    URL noTitleHTMLNote = classLoader.getResource("Takeout/Keep/2018-11-25T02_22_31.261-05_00.html");
+    URL TitledHTMLNote = classLoader.getResource("Takeout/Keep/New_Note_with_a_Title.html");
 
     public googleKeepZipExtractorTest() {
         extractor1 = new googleKeepZipExtractor(notKeep.getPath());
@@ -22,6 +28,7 @@ public class googleKeepZipExtractorTest {
         extractor3 = new googleKeepZipExtractor(keepWithData.getPath());
     }
 
+    // Tests to verify valid Keep Notes ZIP files
     @Test
     public void test_keepVerify() throws IOException {
         assertFalse(extractor1.verifyKeepFolder());
@@ -29,4 +36,18 @@ public class googleKeepZipExtractorTest {
         assertTrue(extractor3.verifyKeepFolder());
     }
 
+    // Extracts actual Keep Notes and matches the titles with manually created Keep Note HTML docs.
+    @Test
+    public void test_keepNotePresence() throws IOException {
+        File file1 = new File(noTitleHTMLNote.getPath());
+        Document doc1 = Jsoup.parse(file1, "utf-8");
+        File file2 = new File(TitledHTMLNote.getPath());
+        Document doc2 = Jsoup.parse(file2, "utf-8");
+        extractor1.returnHtmlKeepNotes();
+        extractor2.returnHtmlKeepNotes();
+
+        ArrayList<Document> notes = extractor3.returnHtmlKeepNotes();
+        assertEquals(notes.get(0).title(), doc2.title());
+        assertEquals(notes.get(1).title(), doc1.title());
+    }
 }
